909afac838527caa3e56f51c2dba4202bea4a946
Nichollas <nichollascavalcante@gmail.com>
2026-08-28 19:41:45 -0300

    feat(backend): implement strategy pattern for material calculations

---
+++ backend/src/main/java/br/edu/ifpb/alumigest/budgets/calculator/CategoryType.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.alumigest.budgets.calculator;
+
+public enum CategoryType {
+    GLASS,
+    PROFILE,
+    HARDWARE,
+    FILM
+}
---
+++ backend/src/main/java/br/edu/ifpb/alumigest/budgets/calculator/TemplateType.java
@@ -0,0 +1,24 @@
+package br.edu.ifpb.alumigest.budgets.calculator;
+
+public enum TemplateType {
+
+    // Linha Correr
+    SLIDING_1_LEAF,
+    SLIDING_2_LEAF,
+    SLIDING_4_LEAF,
+
+    // Linha Giro (incluindo guarda-roupas/móveis)
+    SWING_1_LEAF,
+    SWING_2_LEAF,
+
+    // Linha Basculante/Maxim-ar
+    MAX_AR_WINDOW_1_LEAF,
+    MAX_AR_WINDOW_INVERSE_1_LEAF,
+
+    // Linha Gaveta (Frentes de Alumínio)
+    DRAWER_FRONT,
+
+    // Fixos
+    FIXED_PANEL
+
+}
