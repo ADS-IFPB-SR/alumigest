9cbf9573280c1a1f95569be8aa70955cbd441a0f
Nichollas <nichollascavalcante@gmail.com>
2026-08-30 09:23:23 -0300

    fix: corrige typo no nome do metodo getCalculator

--- backend/src/main/java/br/edu/ifpb/alumigest/budgets/calculator/MaterialCalculatorFactory.java
+++ backend/src/main/java/br/edu/ifpb/alumigest/budgets/calculator/MaterialCalculatorFactory.java
@@ -1,5 +1,32 @@
 package br.edu.ifpb.alumigest.budgets.calculator;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MaterialCalculatorFactory {
+
+    private final Map<CategoryType, MaterialQuantityCalculator> calculators;
+
+    @Autowired
+    public MaterialCalculatorFactory(List<MaterialQuantityCalculator> calculatorList) {
+
+        this.calculators = calculatorList.stream()
+                .collect(Collectors.toMap(MaterialQuantityCalculator::getCategoryType, Function.identity()));
+    }
+
+    public MaterialQuantityCalculator getCalculator(CategoryType categoryType) {
+        MaterialQuantityCalculator calculator = calculators.get(categoryType);
+
+        if (calculator == null) {
+            throw new IllegalArgumentException("Não existe calculadora para a categoria:" + categoryType);
+        }
+        return calculator;
+    }
 
 }
